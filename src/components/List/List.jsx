6a7b77d43a3f456aefa1d4dc57a4c09849da09b5
Taruun <phantmetic@gmail.com>
2023-07-01 15:09:52 +0530

    [Update]: added filterByDistance new filter

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -21,10 +21,14 @@ function List({
   rating,
 }) {
   const classes = useStyles();
-
+  const [distance, setDistance] = useState(4);
   const [elRefs, setElRefs] = useState([]);
 
+  //Filter places based on distance
+  const filteredPlaces = places?.filter((place) => place.distance <= distance);
+
   useEffect(() => {
+    // Create and initialize refs for each place
     const refs = Array(places?.length)
       .fill()
       .map((_, i) => elRefs[i] || createRef());
@@ -42,16 +46,19 @@ function List({
         </div>
       ) : (
         <>
+          {/* Type filter */}
           <FormControl className={classes.formControl}>
-            <InputLabel> Type</InputLabel>
+            <InputLabel>Type</InputLabel>
             <Select value={type} onChange={(e) => setType(e.target.value)}>
-              <MenuItem value="restaurants">Restraurants</MenuItem>
+              <MenuItem value="restaurants">Restaurants</MenuItem>
               <MenuItem value="hotels">Hotels</MenuItem>
               <MenuItem value="attractions">Attractions</MenuItem>
             </Select>
           </FormControl>
+          
+           {/* Rating filter */}
           <FormControl className={classes.formControl}>
-            <InputLabel> Rating</InputLabel>
+            <InputLabel>Rating</InputLabel>
             <Select value={rating} onChange={(e) => setRating(e.target.value)}>
               <MenuItem value={0}>All</MenuItem>
               <MenuItem value={3}>Above 3.0</MenuItem>
@@ -59,13 +66,37 @@ function List({
               <MenuItem value={4.5}>Above 4.5</MenuItem>
             </Select>
           </FormControl>
+
+           {/* Distance filter */}
+          <FormControl className={classes.formControl}>
+            <InputLabel>Distance (km)</InputLabel>
+            <Select
+              value={distance}
+              onChange={(e) => setDistance(e.target.value)}
+            >
+              <MenuItem value={0}>0 km</MenuItem>
+              <MenuItem value={1}>1 km</MenuItem>
+              <MenuItem value={2}>2 km</MenuItem>
+              <MenuItem value={3}>3 km</MenuItem>
+              <MenuItem value={4}>4 km</MenuItem>
+              <MenuItem value={5}>5 km</MenuItem>
+              <MenuItem value={6}>6 km</MenuItem>
+              <MenuItem value={7}>7 km</MenuItem>
+              <MenuItem value={8}>8 km</MenuItem>
+              <MenuItem value={9}>9 km</MenuItem>
+              <MenuItem value={10}>10 km</MenuItem>
+            </Select>
+          </FormControl>
+          
+           {/* Grid of places */}
           <Grid container spacing={3} className={classes.list}>
-            {places?.map((place, i) => (
+            {filteredPlaces?.map((place, i) => (
               <Grid ref={elRefs[i]} item key={i} xs={12}>
                 <PlaceDetails
                   place={place}
                   selected={Number(childClicked) === i}
                   refProp={elRefs[i]}
+                  distance={distance}
                 />
               </Grid>
             ))}
@@ -77,3 +108,4 @@ function List({
 }
 
 export default List;
+
